f6d65dcf9efe326995e29ae8efe7bb920ed182b9
Straatmongol <99067444@mydavinci.nl>
2021-11-02 11:36:57 +0100

    Tafelx

---
+++ tafelx.py
@@ -0,0 +1,17 @@
+def Printtafel(tafel):
+    print(tafel)
+
+tafel = int(input("Welke tafel wilt u gaan leren?"))
+
+Printtafel(tafel * 1)
+Printtafel(tafel * 2)
+Printtafel(tafel * 3)
+Printtafel(tafel * 4)
+Printtafel(tafel * 5)
+Printtafel(tafel * 6)
+Printtafel(tafel * 7)
+Printtafel(tafel * 8)
+Printtafel(tafel * 9)
+Printtafel(tafel * 10)
+
+  
