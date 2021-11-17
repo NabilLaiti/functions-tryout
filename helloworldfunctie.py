e2a74981cebd6dc334bdb08f6d45af9a2bb8b373
Straatmongol <99067444@mydavinci.nl>
2021-11-17 11:11:37 +0100

    Committryout

---
+++ helloworldfunctie.py
@@ -0,0 +1,5 @@
+def helloWorld(aantal:int):
+  for i in range (0,aantal):
+    print('Hello world!')
+
+helloWorld(3)
---
+++ rekesommen.py
@@ -0,0 +1,50 @@
+Nummer1 = int(input("Wat is nummer 1? "))
+Nummer2 = int(input("Wat is nummer 2? "))
+
+#+ functie
+def som1(number1, number2):
+    antwoord = number1 + number2
+    print (str(number1) ,"+", str(number2) ,"=",  str(antwoord))
+som1(Nummer1 , Nummer2)
+
+Nummer1 = int(input("Wat is nummer 1? "))
+Nummer2 = int(input("Wat is nummer 2? "))
+
+#- functie
+def som2(number1, number2):
+    antwoord = number1 - number2
+    print (str(number1) ,"-", str(number2) ,"=",  str(antwoord))
+som2(Nummer1 , Nummer2)
+
+Nummer1 = int(input("Wat is nummer 1? "))
+Nummer2 = int(input("Wat is nummer 2? "))
+
+#* functie
+def som3(number1, number2):
+    antwoord = number1 * number2
+    print (str(number1) ,"x", str(number2) ,"=",  str(antwoord))
+som3(Nummer1 , Nummer2)
+
+
+Nummer1 = int(input("Wat is nummer 1? "))
+Nummer2 = int(input("Wat is nummer 2? "))
+
+#: functie
+def som4(number1, number2):
+    antwoord = number1 / number2
+    print (str(number1) ,":", str(number2) ,"=",  str(antwoord))
+som4(Nummer1 , Nummer2)
+
+
+#+1
+def som5(number):
+    antwoord = Nummer1 + 1
+    print (str(number) ,"+", str(1) ,"=", str(antwoord))
+som5(Nummer1)
+
+
+#-1
+def som6(number):
+    antwoord = Nummer1 - 1
+    print (str(number) ,"-", str(1) ,"=", str(antwoord))
+som6(Nummer1)
