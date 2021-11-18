2fea9524678869fd5e67f5de60983a29cddbfdff
Straatmongol <99067444@mydavinci.nl>
2021-11-18 09:11:01 +0100

    Allecommits

---
+++ Identiteit.py
@@ -0,0 +1,14 @@
+Opnieuw = 'doorgaan'
+while Opnieuw == "doorgaan":  
+    Naam = input("wat is uw naam? ")
+    Leeftijd = input("Wat is uw leeftijd? ")
+    if Naam == "stop":
+        Opnieuw = "stop"
+    elif Leeftijd == "stop":
+        Opnieuw = "stop"
+    else:
+        def GebruikerNaam(x,y):
+            print ("Hallo", x , "je leeftijd is",y, "jaar")
+        
+
+        GebruikerNaam(Naam,Leeftijd)
