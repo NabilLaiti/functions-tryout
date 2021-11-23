827f836c5b1390ae2578fe38d01ce68065ff621a
Straatmongol <99067444@mydavinci.nl>
2021-11-23 11:03:42 +0100

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+Nummer1 = 0
+Nummer2 = 1
+
+def som(Getal1,Getal2):
+    Doorgaan = 'doorgaan'
+    while Doorgaan == 'doorgaan':
+        Getal2 = Getal1 + Getal2
+        Getal1 = Getal1 + Getal2
+        print (Getal2)
+        print (Getal1)
+        if Getal1 >= 1000:
+            Doorgaan ='stop'
+som(Nummer1, Nummer2)
